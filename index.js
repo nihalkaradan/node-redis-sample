f895d96942296a1cb8907d69f61d331b32ae9442
Nihal Karadan <nihalkaradan@gmail.com>
2021-03-30 16:25:27 +0530

    cache added

--- index.js
+++ index.js
@@ -8,13 +8,36 @@ const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const client = redis.createClient(REDIS_PORT);
 
 const app = express();
+
+//cache middleware
+function cache(req, res, next) {
+  const { username } = req.params;
+  client.get(username, (err, data) => {
+    if (err) res.json(err);
+    if (data !== null) {
+      res.json({
+        username,
+        data,
+      });
+    } else {
+      next();
+    }
+  });
+}
+
+//Make request to github
 const getRepos = async (req, res, next) => {
   try {
     console.log("Fetching");
     const { username } = req.params;
     const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
-    res.send(data);
+    const repos = data.public_repos;
+    client.setex(username, 3600, repos);
+    res.json({
+      username: username,
+      repos: repos,
+    });
   } catch (error) {
     res.status(500).json({
       error,
@@ -22,7 +45,7 @@ const getRepos = async (req, res, next) => {
   }
 };
 
-app.get("/repos/:username", getRepos);
+app.get("/repos/:username", cache, getRepos);
 
 app.listen(PORT, () => {
   console.log(`listening at PORT: ${PORT}`);
