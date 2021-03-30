32be90d41f8b7fc18be11ad7c08fb4fe3f28f670
Nihal Karadan <nihalkaradan@gmail.com>
2021-03-30 13:20:41 +0530

    github fetch

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+
+const app = express();
+const getRepos = async (req, res, next) => {
+  try {
+    console.log("Fetching");
+    const { username } = req.params;
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    res.status(500).json({
+      error,
+    });
+  }
+};
+
+app.get("/repos/:username", getRepos);
+
+app.listen(PORT, () => {
+  console.log(`listening at PORT: ${PORT}`);
+});
